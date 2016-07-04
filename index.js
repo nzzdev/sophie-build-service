0698ffe572cd7418932c6970612d745b6c412923
Beni <beni.buess@nzz.ch>
2016-07-04 17:52:00 +0200

    allows submodules bundling, v 0.2.0

--- index.js
+++ index.js
@@ -20,15 +20,29 @@ server.route({
 
     const packages = request.params.packages.split(',')
       .map(p => {
+        let submodules;
+        if (p.split('[')[0] !== p) {
+          submodules = p.split('[')[1].replace(']','');
+        }
         return {
-          name: p.split('@')[0],
-          version: p.split('@')[1]
+          name: p.split('[')[0].split('@')[0],
+          version: p.split('[')[0].split('@')[1],
+          submodules: typeof submodules === 'string' ? submodules.split('+') : undefined
         }
       })
 
     loadSophieBundle(packages)
-      .then(styles => {
-        reply(styles).type('text/css');
+      .then(bundle => {
+        const response = reply(bundle.styles)
+
+        response
+          .type('text/css')
+          .ttl(24 * 60 * 60 * 1000)
+
+        if (bundle.lastModified) {
+          response.header('Last-Modified', bundle.lastModified.toUTCString())
+        }
+
       })
       .catch(err => {
         console.log('err', err)
