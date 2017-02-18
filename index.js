40892b60fed6ea9662f5f19d302f9dee3dc4d878
Beni <beni.buess@nzz.ch>
2017-02-19 00:23:22 +0100

    adds /health endpoint for status check

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
 const Hoek = require('hoek');
+const Boom = require('boom');
 const server = require('./server.js');
 const routes = require('./routes/routes.js')
+const pusage = require('pidusage')
 
-const plugins = [];
+const plugins = [
+  {
+    register: require('hapi-alive'),
+    options: {
+      path: '/health',
+      tags: ['health', 'monitor'],
+      healthCheck: function(server, callback) {
+        pusage.stat(process.pid, function(err, stat) {
+          var error;
+          if (stat.cpu > 90) {
+            error = new Boom.internal('load is too high');
+          }
+          if ((stat.memory / 1000000) > 1000) {
+            error = new Boom.internal('memory usage is too high');
+          }
+          callback(error);
+        })
+      }
+    }
+  }
+];
 
 server.register(plugins, err => {
   Hoek.assert(!err, err);
