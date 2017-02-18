2661152c0e98e7b77cba111b00be3710f93cc898
Beni <beni.buess@nzz.ch>
2017-02-18 22:36:56 +0100

    explicitly configure catbox-memory cache

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const Hapi = require('hapi');
 
-const server = new Hapi.Server();
+const server = new Hapi.Server({
+  cache: [
+    {
+      engine: require('catbox-memory'),
+      options: {
+        maxByteSize: 1000000000, // ~ 1GB
+      }
+    },
+  ]
+});
+
 server.connection({
   port: process.env.PORT || 3000,
   routes: {
