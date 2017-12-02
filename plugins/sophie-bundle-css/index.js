de7dbd0cb8bdbca88c38335c0a1ebe7e5cde9edf
benib <beni.buess@nzz.ch>
2017-12-02 14:27:44 +0100

    bundle generate timeout set to 1 minute

--- plugins/sophie-bundle-css/index.js
+++ plugins/sophie-bundle-css/index.js
@@ -9,7 +9,7 @@ const defaultServerMethodCaching = {
   expiresIn: 48 * 60 * 60 * 1000, // expire after 48 hours
   staleIn: 1 * 60 * 5 * 1000, // rebuild bundles every 5 minutes on request
   staleTimeout: 1, // do not wait before returning a stale bundle
-  generateTimeout: 30 * 60 * 1000 // 30 minutes
+  generateTimeout: 60 * 1000 // 1 minute
 }
 
 module.exports = {
