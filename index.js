e2befdde28cf974071480abd7a0ff2fa46c95b3f
Manuel Roth <manuelroth@hotmail.ch>
2021-10-05 16:26:00 +0200

    Improve logging

--- index.js
+++ index.js
@@ -5,10 +5,11 @@ const path = require("path");
 const cacheControl = [
   "public",
   `max-age=${process.env.CACHE_CONTROL_MAX_AGE || 43200}`, // 12 hours
-  `stale-while-revalidate=${process.env.CACHE_CONTROL_STALE_WHILE_REVALIDATE ||
-    604800}`, // 7 days
+  `stale-while-revalidate=${
+    process.env.CACHE_CONTROL_STALE_WHILE_REVALIDATE || 604800
+  }`, // 7 days
   `stale-if-error=${process.env.CACHE_CONTROL_STALE_IF_ERROR || 604800}`, // 7 days
-  `s-maxage=${process.env.CACHE_CONTROL_S_MAXAGE || 300}` // 5 minutes
+  `s-maxage=${process.env.CACHE_CONTROL_S_MAXAGE || 300}`, // 5 minutes
 ];
 
 async function start() {
@@ -19,21 +20,21 @@ async function start() {
         provider: {
           constructor: require("@hapi/catbox-memory"),
           options: {
-            maxByteSize: 1000000000 // ~ 1GB
-          }
-        }
-      }
+            maxByteSize: 1000000000, // ~ 1GB
+          },
+        },
+      },
     ],
     routes: {
       cors: true,
       files: {
-        relativeTo: path.join(__dirname, "public")
+        relativeTo: path.join(__dirname, "public"),
       },
-      state: { parse: false, failAction: "log" }
+      state: { parse: false, failAction: "log" },
     },
     app: {
-      cacheControl: cacheControl.join(", ")
-    }
+      cacheControl: cacheControl.join(", "),
+    },
   });
 
   await server.register(require("@hapi/inert"));
@@ -42,37 +43,43 @@ async function start() {
     plugin: require("./plugins/sophie-package-loader-github/index.js"),
     options: {
       githubUserName: process.env.GITHUB_USER_NAME,
-      githubAuthToken: process.env.GITHUB_AUTH_TOKEN
-    }
+      githubAuthToken: process.env.GITHUB_AUTH_TOKEN,
+    },
   });
 
   await server.register({
-    plugin: require("./plugins/sophie-bundle/index.js")
+    plugin: require("./plugins/sophie-bundle/index.js"),
   });
 
   await server.register({
     plugin: require("./plugins/sophie-bundle-css/index.js"),
     options: {
-      tmpDir: path.join(__dirname, "/tmp")
-    }
+      tmpDir: path.join(__dirname, "/tmp"),
+    },
   });
 
   await server.register({
     plugin: require("./plugins/sophie-bundle-vars-json/index.js"),
     options: {
-      tmpDir: path.join(__dirname, "/tmp")
-    }
+      tmpDir: path.join(__dirname, "/tmp"),
+    },
   });
 
   await server.register({
     plugin: require("hapi-pino"),
     options: {
+      redact: ["req.headers.authorization", "req.headers.cookie"],
       prettyPrint:
         process.env.APP_ENV !== "production" &&
-        process.env.APP_ENV !== "staging",
+        process.env.APP_ENV !== "staging" &&
+        process.env.APP_ENV !== "test" &&
+        process.env.APP_ENV !== "local",
+      logRouteTags: true,
+      // This is required otherwise the request object will be logged twice
+      // See https://github.com/pinojs/hapi-pino/pull/92 for more details
+      getChildBindings: () => ({}),
       ignorePaths: ["/health"],
-      mergeHapiLogData: true
-    }
+    },
   });
 
   server.route(routes);
@@ -84,9 +91,9 @@ async function start() {
       directory: {
         path: ".",
         redirectToSlash: true,
-        index: true
-      }
-    }
+        index: true,
+      },
+    },
   });
 
   await server.start();
