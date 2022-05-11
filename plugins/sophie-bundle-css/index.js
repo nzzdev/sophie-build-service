7223ca18a941ba43c86e9474dd80c605f34d19d3
Nicolas Staub <nicolas.staub@nzz.ch>
2022-05-11 09:11:03 +0200

    add new server method sophie.processPackage for better caching, disable caching on sophie.generateBundle.css (for testing)

--- plugins/sophie-bundle-css/index.js
+++ plugins/sophie-bundle-css/index.js
@@ -11,114 +11,107 @@ const defaultServerMethodCaching = {
   generateTimeout: 60 * 1000, // 1 minute
 };
 
+function compileStyle(package, loadPath, filesToCompile) {
+  let compiledStyles = "", fileName;
+
+  while ((fileName = filesToCompile.shift())) {
+    // server.log(["debug"], `compiling styles ${fileName} of ${package.name}`);
+    let rendered;
+
+    try {
+      rendered = sass.renderSync({
+        file: path.join(loadPath, fileName),
+        includePaths: [
+          path.join(loadPath, "sophie_packages"),
+        ],
+        importer: [jsonImporter()],
+        outputStyle: "compressed",
+      });
+    } catch (err) {
+      throw Boom.badImplementation(
+        `sass compilation error in package
+        ${package.name} file ${fileName}: ${err.message}`
+      );
+    }
+
+    compiledStyles += rendered.css.toString();
+    // server.log(["debug"], `compiled styles ${fileName} of ${package.name}`);
+  }
+  return compiledStyles;
+}
+
 module.exports = {
   name: "sophie-bundle-css",
   register: async function (server, options) {
     Hoek.assert(options.tmpDir, "tmpDir is a required option");
 
-    // $lab:coverage:off$
     const cacheConfig = Hoek.applyToDefaults(
       defaultServerMethodCaching,
       options.serverCacheConfig || {}
     );
-    // $lab:coverage:on$
+
+    server.method(
+      "sophie.processPackage",
+      async function (package, pathPrefix) {
+        // download GitHub repository tarball for this package and save it to 'loadPath'
+        const loadPath = path.join(
+          pathPrefix,
+          package.name,
+          package.version || package.branch
+        );
+        await server.methods.sophie.loadPackage(package, loadPath);
+
+        // if this package has submodules, we need to compile them as well
+        let filesToCompile = [];
+        if (!package.submodules) {
+          // if no submodules are given, we compile all submodules
+          // check if there is the scss directory first to not fail if a module has no submodules
+          const submodulePath = path.join(
+            pathPrefix,
+            package.name,
+            package.version || package.branch,
+            "scss"
+          );
+          if (fs.existsSync(submodulePath)) {
+            const submoduleFiles = fs.readdirSync(submodulePath);
+            filesToCompile = submoduleFiles.map((file) => `scss/${file}`);
+          } else {
+            // this is mostly a fallback for older style sophie modules that just have a main.scss file
+            filesToCompile = ["main.scss"];
+          }
+        } else {
+          filesToCompile = package.submodules.map((sm) => `scss/${sm}.scss`);
+        }
+
+        // compile all sass from this package and its submodules
+        return compileStyle(package, loadPath, filesToCompile);
+      },
+      {
+        cache: cacheConfig,
+        generateKey: (package) => package.name + "/" + package.version,
+      }
+    )
 
     server.method(
       "sophie.generateBundle.css",
       async function (bundleId) {
-        const packages =
-          server.methods.sophie.bundle.getPackagesFromBundleId(bundleId);
+        const packages = server.methods.sophie.bundle.getPackagesFromBundleId(bundleId);
         const packagesHash = crypto
           .createHash("md5")
           .update(bundleId)
           .digest("hex");
         const pathPrefix = path.join(options.tmpDir, packagesHash);
-
-        await server.methods.sophie.loadPackages(packages, pathPrefix);
-        server.log(["debug"], `got all packages ready at ${options.tmpDir}`);
-
         let compiledStyles = "";
-        for (const pack of packages) {
-          const packageInfo = JSON.parse(
-            fs.readFileSync(
-              path.join(
-                pathPrefix,
-                pack.name,
-                pack.version || pack.branch,
-                "package.json"
-              )
-            )
-          );
-          const sophiePackageInfo = packageInfo.sophie || {};
 
-          let filesToCompile = [];
-          if (!pack.submodules) {
-            // if no submodules are given, we compile all submodules
-            // check if there is the scss directory first to not fail if a module has no submodules
-            const submodulePath = path.join(
-              pathPrefix,
-              pack.name,
-              pack.version || pack.branch,
-              "scss"
-            );
-            if (fs.existsSync(submodulePath)) {
-              const submoduleFiles = fs.readdirSync(submodulePath);
-              filesToCompile = submoduleFiles.map((file) => `scss/${file}`);
-            } else {
-              // this is mostly a fallback for older style sophie modules that just have a main.scss file
-              filesToCompile = ["main.scss"];
-            }
-          } else {
-            filesToCompile = pack.submodules.map((sm) => `scss/${sm}.scss`);
-          }
-
-          let fileName;
-          while ((fileName = filesToCompile.shift())) {
-            server.log(
-              ["debug"],
-              `compiling styles ${fileName} of ${pack.name}`
-            );
-            let rendered;
-            try {
-              rendered = sass.renderSync({
-                file: path.join(
-                  pathPrefix,
-                  pack.name,
-                  pack.version || pack.branch,
-                  fileName
-                ),
-                includePaths: [
-                  path.join(
-                    pathPrefix,
-                    pack.name,
-                    pack.version || pack.branch,
-                    "sophie_packages"
-                  ),
-                ],
-                importer: [jsonImporter()],
-                outputStyle: "compressed",
-              });
-            } catch (err) {
-              throw Boom.badImplementation(
-                `sass compilation error in package
-                ${pack.name} file ${fileName}: ${err.message}`
-              );
-            }
-
-            const styles = rendered.css.toString();
-            server.log(
-              ["debug"],
-              `compiled styles ${fileName} of ${pack.name}`
-            );
-            compiledStyles += styles;
-          }
+        for (const package of packages) {
+          compiledStyles += await server.methods.sophie.processPackage(package, pathPrefix);
         }
 
         return compiledStyles;
       },
-      {
-        cache: cacheConfig,
-      }
+      // {
+      //   cache: cacheConfig,
+      // }
     );
   },
 };
